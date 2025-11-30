1a9e972bd63b0fd4882d3167e3d4a1a32aaf9663
AzizBenIsmail <aziz.270700@gmail.com>
2025-11-30 13:11:14 +0100

    séance 4

--- src/components/Cards/CardTableUsers.js
+++ src/components/Cards/CardTableUsers.js
@@ -1,23 +1,67 @@
-import React , {useState , useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import { getAllUsers , deleteUser } from "../../services/apiUser";
+import {
+  getAllUsers,
+  deleteUser,
+  addNewUser,
+  updateUser,
+  addNewUserWithImg,
+} from "../../services/apiUser";
 
 export default function CardTable({ color }) {
-
   const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    age: 0,
+    password: "",
+    user_image: "null",
+  });
+  const [image, setImage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    // if (name === "email") {
+    //   console.log("Email changed:", value);
+    //   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    //   setErrorMessage({
+    //     ...errorMessage,
+    //     email: emailRegex.test(value) ? "" : "Invalid email format",
+    //   });
+    // }
+
+    setNewUser({
+      ...newUser,
+      [name]: value,
+    });
+    console.log(newUser);
+  };
 
   const fetchUsers = async () => {
     try {
       const response = await getAllUsers();
       setUsers(response.data.userList);
-    }
-    catch (error) {
+    } catch (error) {
       console.error("Error fetching users:", error);
     }
   };
 
+  const [count, setCount] = useState(0);
+
   useEffect(() => {
     fetchUsers();
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+      fetchUsers();
+    }, 5000); // Fetch users every 5 seconds
+
+    return () => clearInterval(interval); // Cleanup on unmount
   }, []);
 
   const handleDelete = async (id) => {
@@ -26,6 +70,56 @@ export default function CardTable({ color }) {
       fetchUsers(); // Refresh the user list after deletion
     } catch (error) {
       console.error("Error deleting user:", error);
+    }
+  };
+
+  const handleAddUser = async () => {
+    try {
+      if (!newUser.email.includes("@")) {
+        setErrorMessage({
+          ...errorMessage,
+          email: "Invalid email format",
+        });
+      }
+      // Implement user addition logic here
+      await addNewUser(newUser);
+      fetchUsers(); // Refresh the user list after adding a new user
+    } catch (error) {
+      console.error("Error adding user:", error);
+    }
+  };
+
+  const handleUpdateUser = async () => {
+    try {
+      console.log("updating user:", newUser);
+      // Implement user addition logic here
+      await updateUser(newUser._id, newUser);
+      fetchUsers(); // Refresh the user list after adding a new user
+    } catch (error) {
+      console.error("Error adding user:", error);
+    }
+  };
+
+  const handleChangeFile = (e) => {
+    setImage(e.target.files[0]);
+  };
+  const formData = new FormData();
+
+  const handleAddUserWithImg = async () => {
+    try {
+      // Implement user addition logic here
+
+      formData.append("firstName", newUser.firstName);
+      formData.append("lastName", newUser.lastName);
+      formData.append("email", newUser.email);
+      formData.append("age", newUser.age);
+      formData.append("password", newUser.password);
+      formData.append("user_image", image);
+
+      await addNewUserWithImg(formData);
+      fetchUsers(); // Refresh the user list after adding a new user
+    } catch (error) {
+      console.error("Error adding user:", error);
     }
   };
   return (
@@ -45,7 +139,7 @@ export default function CardTable({ color }) {
                   (color === "light" ? "text-blueGray-700" : "text-white")
                 }
               >
-                List des utilisateur 
+                List des utilisateur
               </h3>
             </div>
           </div>
@@ -63,7 +157,7 @@ export default function CardTable({ color }) {
                       : "bg-lightBlue-800 text-lightBlue-300 border-lightBlue-700")
                   }
                 >
-                   First Name Last Name
+                  First Name Last Name
                 </th>
                 <th
                   className={
@@ -116,46 +210,148 @@ export default function CardTable({ color }) {
               </tr>
             </thead>
             <tbody>
-              {users.map((user,index)=>(
-              <tr>
-                <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
-                  <img
-                    src={`http://localhost:5000/images/Users/${user.user_image}`}
-                    className="h-12 w-12 bg-white rounded-full border"
-                    alt="..."
-                  ></img>{" "}
-                  <span
-                    className={
-                      "ml-3 font-bold " +
-                      +(color === "light" ? "text-blueGray-600" : "text-white")
-                    }
-                  >
-                   { user.firstName}  {user.lastName}
-                  </span>
-                </th>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  {user.email}
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  {user.age}
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div className="flex">
-                    {user.roles}
-                  </div>
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div className="flex items-center">
-                    <button className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Update</button>
-                    <button className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" 
-                    onClick = {()=> handleDelete(user._id)} type="button">Delete</button>
-                  </div>
-                </td>
-              </tr>
+              {users.map((user, index) => (
+                <tr>
+                  <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
+                    <img
+                      src={`http://localhost:5000/images/Users/${user.user_image}`}
+                      className="h-12 w-12 bg-white rounded-full border"
+                      alt="..."
+                    ></img>{" "}
+                    <span
+                      className={
+                        "ml-3 font-bold " +
+                        +(color === "light"
+                          ? "text-blueGray-600"
+                          : "text-white")
+                      }
+                    >
+                      {user.firstName} {user.lastName}
+                    </span>
+                  </th>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    {user.email}
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    {user.age}
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    <div className="flex">{user.roles}</div>
+                  </td>
+                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                    <div className="flex items-center">
+                      <button
+                        className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        type="button"
+                        onClick={() => {
+                          setNewUser(user);
+                          console.log(user);
+                        }}
+                      >
+                        Update
+                      </button>
+                      <button
+                        className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        onClick={() => handleDelete(user._id)}
+                        type="button"
+                      >
+                        Delete
+                      </button>
+                      <button
+                        className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        onClick={() => handleDelete(user._id)}
+                        type="button"
+                      >
+                        updatePassword
+                      </button>
+                    </div>
+                  </td>
+                </tr>
               ))}
             </tbody>
           </table>
         </div>
+      </div>
+      <div>
+        <input
+          type="text"
+          name="firstName"
+          placeholder="First Name"
+          class="px-3 py-3 ml-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-20"
+          value={newUser.firstName}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Last Name"
+          class="px-3 py-3 ml-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-20"
+          value={newUser.lastName}
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          class="px-3 py-3 ml-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-20"
+          value={newUser.email}
+          onChange={handleChange}
+        />
+        {errorMessage.email && (
+          <div class="text-red-500 text-xs italic mt-1 ml-3">
+            {errorMessage.email}
+          </div>
+        )}
+        <input
+          type="number"
+          name="age"
+          placeholder="Age"
+          class="px-3 py-3 ml-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-20"
+          value={newUser.age}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          class="px-3 py-3 ml-3 mt-2 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-20"
+          // value={newUser.password}
+          onChange={handleChange}
+        />
+        <input
+          type="file"
+          name="firstName"
+          placeholder="First Name"
+          class="px-3 py-3 ml-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-20"
+          onChange={handleChangeFile}
+        />
+        <button
+          onClick={() => {
+            handleAddUser();
+          }}
+          class="bg-lightBlue-500 ml-2 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 mt-2"
+          type="button"
+        >
+          Add User
+        </button>
+        <button
+          onClick={() => {
+            handleAddUserWithImg();
+          }}
+          class="bg-lightBlue-500 ml-2 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 mt-2"
+          type="button"
+        >
+          Add User With Img
+        </button>
+        <button
+          onClick={() => {
+            handleUpdateUser();
+          }}
+          class="bg-lightBlue-500 ml-2 text-white active:bg-lightBlue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 mt-2"
+          type="button"
+        >
+          Update User
+        </button>
       </div>
     </>
   );
--- src/services/apiUser.js
+++ src/services/apiUser.js
@@ -1,11 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:5000/users';
+const API_URL = "http://localhost:5000/users";
 
-export async function getAllUsers(){
-    return await axios.get(`${API_URL}/getAllUsers`)
+export async function getAllUsers() {
+  return await axios.get(`${API_URL}/getAllUsers`);
 }
 
-export async function deleteUser(id){
-    return await axios.delete(`${API_URL}/deleteUser/${id}`,id)
+export async function deleteUser(id) {
+  return await axios.delete(`${API_URL}/deleteUser/${id}`, id);
+}
+
+export async function addNewUser(userData) {
+  return await axios.post(`${API_URL}/addUser`, userData);
+}
+
+export async function updateUser(id, userData) {
+  return await axios.put(`${API_URL}/updateUser/${id}`, userData);
+}
+
+export async function addNewUserWithImg(userData) {
+  return await axios.post(`${API_URL}/addClient`, userData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
 }
